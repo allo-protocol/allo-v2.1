1cc8ccec6c4e590a0767f0cc4ae858584e42b316
ilpepepig <ilpepepig@defi.sucks>
2024-09-17 16:13:59 -0300

    chore: support strategy clones and strategy implementations

--- script/core/interaction/CreatePool.sol
+++ script/core/interaction/CreatePool.sol
@@ -5,16 +5,10 @@ import {Script, console} from "forge-std/Script.sol";
 
 import {Metadata} from "contracts/core/libraries/Metadata.sol";
 import {Allo} from "contracts/core/Allo.sol";
-import {IRegistry} from "contracts/core/interfaces/IRegistry.sol";
 
 contract CreatePool is Script {
-    // Define the following parameters for the new profile.
-    uint256 public nonce = uint256(0);
-    string public name = "";
-    Metadata public metadata = Metadata({protocol: uint256(0), pointer: ""});
-    address public owner = address(0);
-    address[] public members = [];
-
+    // Define the following parameters for the new pool.
+    bool public cloneStrategy = true;
     uint256 public msgValue = uint256(0);
     bytes32 public profileId = bytes32(0);
     address public strategy = address(0);
@@ -35,7 +29,14 @@ contract CreatePool is Script {
 
     function _createPool() internal returns (uint256 poolId) {
         Allo allo = Allo(vm.envAddress("ALLO_ADDRESS"));
-        poolId =
-            allo.createPool{value: msgValue}(profileId, strategy, initStrategyData, token, amount, metadata, managers);
+        if (cloneStrategy) {
+            poolId = allo.createPool{value: msgValue}(
+                profileId, strategy, initStrategyData, token, amount, metadata, managers
+            );
+        } else {
+            poolId = allo.createPoolWithCustomStrategy{value: msgValue}(
+                profileId, strategy, initStrategyData, token, amount, metadata, managers
+            );
+        }
     }
 }
