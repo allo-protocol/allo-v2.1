3a2667badf2f980b99a241696a04539ecf9c0035
0xAustrian <austrian@defi.sucks>
2024-09-18 17:36:31 -0300

    test: add a few more test cases

--- test/unit/strategies/BaseStrategyUnit.t.sol
+++ test/unit/strategies/BaseStrategyUnit.t.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {MockMockBaseStrategy} from "test/smock/MockMockBaseStrategy.sol";
 import {IBaseStrategy} from "strategies/IBaseStrategy.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IAllo} from "contracts/core/interfaces/IAllo.sol";
 
 contract BaseStrategy is Test {
     MockMockBaseStrategy baseStrategy;
+    address constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     function setUp() public {
         baseStrategy = new MockMockBaseStrategy(address(0));
@@ -91,118 +94,167 @@ contract BaseStrategy is Test {
         baseStrategy.increasePoolAmount(_amount);
     }
 
-    function test_WithdrawShouldCallOnlyPoolManager() external {
+    function test_WithdrawWhenParametersAreValid(address _token, uint256 _amount, address _recipient) external {
+        // TODO: Fix the bug and fix this test
+        vm.skip(true);
+
+        vm.assume(_token != NATIVE);
+        baseStrategy.mock_call__checkOnlyPoolManager(address(this));
+
+        vm.mockCall(_token, abi.encodeWithSelector(IERC20.balanceOf.selector), abi.encode(type(uint256).max));
+        vm.mockCall(_token, abi.encodeWithSelector(IERC20.transfer.selector), abi.encode(true));
+
         // It should call onlyPoolManager
-        vm.skip(true);
-    }
+        baseStrategy.expectCall__checkOnlyPoolManager(address(this));
 
-    function test_WithdrawShouldCall_beforeWithdraw() external {
         // It should call _beforeWithdraw
-        vm.skip(true);
-    }
+        baseStrategy.expectCall__beforeWithdraw(_token, _amount, _recipient);
 
-    function test_WithdrawShouldCallBalanceOfAtToken() external {
         // It should call balanceOf at token
-        vm.skip(true);
-    }
+        vm.expectCall(_token, abi.encodeWithSelector(IERC20.balanceOf.selector, address(baseStrategy)));
 
-    function test_WithdrawRevertWhen_AmountIsGreaterThanPoolAmount() external {
-        // It should revert
-        vm.skip(true);
-    }
-
-    function test_WithdrawShouldCallTransferAtToken() external {
         // It should call transfer at token
-        vm.skip(true);
-    }
+        vm.expectCall(_token, abi.encodeWithSelector(IERC20.transfer.selector, _recipient, _amount));
 
-    function test_WithdrawShouldCall_afterWithdraw() external {
         // It should call _afterWithdraw
-        vm.skip(true);
-    }
+        baseStrategy.expectCall__afterWithdraw(_token, _amount, _recipient);
 
-    function test_WithdrawShouldEmitEvent() external {
         // It should emit event
-        vm.skip(true);
+        vm.expectEmit();
+        emit IBaseStrategy.Withdrew(_token, _amount, _recipient);
+
+        baseStrategy.withdraw(_token, _amount, _recipient);
     }
 
-    function test_RegisterShouldCallOnlyAllo() external {
-        // It should call onlyAllo
+    function test_WithdrawRevertWhen_AmountIsGreaterThanPoolAmount(
+        uint256 _poolAmount,
+        address _token,
+        uint256 _amount,
+        address _recipient
+    ) external {
+        // TODO: Fix the bug and fix this test
         vm.skip(true);
-    }
 
-    function test_RegisterShouldCall_beforeRegisterRecipient() external {
-        // It should call _beforeRegisterRecipient
-        vm.skip(true);
-    }
+        vm.assume(_token != NATIVE);
+        vm.assume(_amount > _poolAmount);
+        baseStrategy.mock_call__checkOnlyPoolManager(address(this));
+        baseStrategy.set__poolAmount(_poolAmount);
 
-    function test_RegisterShouldCall_register() external {
-        // It should call _register
-        vm.skip(true);
-    }
+        vm.mockCall(_token, abi.encodeWithSelector(IERC20.balanceOf.selector), abi.encode(_poolAmount));
 
-    function test_RegisterShouldCall_afterRegisterRecipient() external {
-        // It should call _afterRegisterRecipient
-        vm.skip(true);
-    }
-
-    function test_RegisterShouldReturn_recipientIds() external {
-        // It should return _recipientIds
-        vm.skip(true);
-    }
-
-    function test_AllocateShouldCallOnlyAllo() external {
-        // It should call onlyAllo
-        vm.skip(true);
-    }
-
-    function test_AllocateShouldCall_beforeAllocate() external {
-        // It should call _beforeAllocate
-        vm.skip(true);
-    }
-
-    function test_AllocateShouldCall_allocate() external {
-        // It should call _allocate
-        vm.skip(true);
-    }
-
-    function test_AllocateShouldCall_afterAllocate() external {
-        // It should call _afterAllocate
-        vm.skip(true);
-    }
-
-    function test_DistributeShouldCallOnlyAllo() external {
-        // It should call onlyAllo
-        vm.skip(true);
-    }
-
-    function test_DistributeShouldCall_beforeDistribute() external {
-        // It should call _beforeDistribute
-        vm.skip(true);
-    }
-
-    function test_DistributeShouldCall_distribute() external {
-        // It should call _distribute
-        vm.skip(true);
-    }
-
-    function test_DistributeShouldCall_afterDistribute() external {
-        // It should call _afterDistribute
-        vm.skip(true);
-    }
-
-    function test__checkOnlyAlloRevertWhen_CallerIsNotAllo() external {
         // It should revert
-        vm.skip(true);
+        vm.expectRevert(IBaseStrategy.BaseStrategy_WITHDRAW_MORE_THAN_POOL_AMOUNT.selector);
+
+        baseStrategy.withdraw(_token, _amount, _recipient);
+    }
+
+    function test_RegisterWhenParametersAreValid(address[] memory _recipients, bytes memory _data, address _sender)
+        external
+    {
+        baseStrategy.mock_call__checkOnlyAllo();
+        baseStrategy.mock_call__register(_recipients, _data, _sender, _recipients);
+
+        // It should call onlyAllo
+        baseStrategy.expectCall__checkOnlyAllo();
+
+        // It should call _beforeRegisterRecipient
+        baseStrategy.expectCall__beforeRegisterRecipient(_recipients, _data, _sender);
+
+        // It should call _register
+        baseStrategy.expectCall__register(_recipients, _data, _sender);
+
+        // It should call _afterRegisterRecipient
+        baseStrategy.expectCall__afterRegisterRecipient(_recipients, _data, _sender);
+
+        address[] memory _recipientIds = baseStrategy.register(_recipients, _data, _sender);
+
+        // It should return _recipientIds
+        for (uint256 i = 0; i < _recipientIds.length; i++) {
+            assertEq(_recipientIds[i], _recipients[i]);
+        }
+    }
+
+    function test_AllocateWhenParametersAreValid(
+        address[] memory _recipients,
+        uint256[] memory _amounts,
+        bytes memory _data,
+        address _sender
+    ) external {
+        baseStrategy.mock_call__checkOnlyAllo();
+        baseStrategy.mock_call__allocate(_recipients, _amounts, _data, _sender);
+
+        // It should call onlyAllo
+        baseStrategy.expectCall__checkOnlyAllo();
+
+        // It should call _beforeAllocate
+        baseStrategy.expectCall__beforeAllocate(_recipients, _data, _sender);
+
+        // It should call _allocate
+        baseStrategy.expectCall__allocate(_recipients, _amounts, _data, _sender);
+
+        // It should call _afterAllocate
+        baseStrategy.expectCall__afterAllocate(_recipients, _data, _sender);
+
+        baseStrategy.allocate(_recipients, _amounts, _data, _sender);
+    }
+
+    function test_DistributeWhenParametersAreValid(address[] memory _recipientIds, bytes memory _data, address _sender)
+        external
+    {
+        baseStrategy.mock_call__checkOnlyAllo();
+        baseStrategy.mock_call__distribute(_recipientIds, _data, _sender);
+
+        // It should call onlyAllo
+        baseStrategy.expectCall__checkOnlyAllo();
+
+        // It should call _beforeDistribute
+        baseStrategy.expectCall__beforeDistribute(_recipientIds, _data, _sender);
+
+        // It should call _distribute
+        baseStrategy.expectCall__distribute(_recipientIds, _data, _sender);
+
+        // It should call _afterDistribute
+        baseStrategy.expectCall__afterDistribute(_recipientIds, _data, _sender);
+
+        baseStrategy.distribute(_recipientIds, _data, _sender);
+    }
+
+    function test__checkOnlyAlloRevertWhen_CallerIsNotAllo(address _caller) external {
+        vm.assume(_caller != address(baseStrategy.getAllo()));
+
+        // It should revert
+        vm.expectRevert(IBaseStrategy.BaseStrategy_UNAUTHORIZED.selector);
+
+        vm.prank(_caller);
+        baseStrategy.call__checkOnlyAllo();
     }
 
     function test__checkOnlyPoolManagerShouldCallIsPoolManagerAtAllo() external {
+        vm.mockCall(
+            address(baseStrategy.getAllo()),
+            abi.encodeWithSelector(IAllo.isPoolManager.selector, baseStrategy.getPoolId(), address(this)),
+            abi.encode(true)
+        );
+
         // It should call isPoolManager at allo
-        vm.skip(true);
+        vm.expectCall(
+            address(baseStrategy.getAllo()),
+            abi.encodeWithSelector(IAllo.isPoolManager.selector, baseStrategy.getPoolId(), address(this))
+        );
+
+        baseStrategy.call__checkOnlyPoolManager(address(this));
     }
 
     function test__checkOnlyPoolManagerRevertWhen_CallerIsNotPoolManager() external {
+        vm.mockCall(
+            address(baseStrategy.getAllo()),
+            abi.encodeWithSelector(IAllo.isPoolManager.selector, baseStrategy.getPoolId(), address(this)),
+            abi.encode(false)
+        );
+
         // It should revert
-        vm.skip(true);
+        vm.expectRevert(IBaseStrategy.BaseStrategy_UNAUTHORIZED.selector);
+
+        baseStrategy.call__checkOnlyPoolManager(address(this));
     }
 }
