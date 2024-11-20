c0ffee3be1fc1cf4b30c1a8367047bfc82c14c79
drgorillamd <83670532+drgorillamd@users.noreply.github.com>
2024-11-20 13:05:46 +0100

    test(medusa): prop 18

--- test/invariant/fuzz/properties/PropertiesAllo.t.sol
+++ test/invariant/fuzz/properties/PropertiesAllo.t.sol
@@ -576,6 +576,8 @@ contract PropertiesAllo is HandlersParent {
         }
     }
 
+    ///@custom:property-id 18
+    ///@custom:property anyone can increase fund in a pool, if strategy (hook) logic allows so and if more than base fee
     ///@custom:property-id 19
     ///@custom:property every deposit/pool creation must take the correct fee on the amount deposited, forwarded to the treasury
     function prop_anyoneCanIncreaseFundInAPool(
@@ -619,10 +621,17 @@ contract PropertiesAllo is HandlersParent {
                 _previousBalanceStrategy + _amountAfterFee,
                 "property-id 18: increasePoolFunds invalid strategy balance"
             );
+
             assertEq(
                 _afterBalanceTreasury,
                 _previousBalanceTreasury + _feeAmount,
                 "property-id 19: increasePoolFunds invalid treasury balance"
+            );
+
+            assertGt(
+                _amount,
+                allo.getBaseFee(),
+                "property-id 18: increasePoolFunds failed"
             );
         } else {
             (
@@ -642,6 +651,7 @@ contract PropertiesAllo is HandlersParent {
                 _amount == 0 ||
                     (_successAllocationEndtime &&
                         _allocationEndTime < block.timestamp),
+                    || _amount < allo.getBaseFee(),
                 "property-id 18: increasePoolFunds failed"
             );
         }
