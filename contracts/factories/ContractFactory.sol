b859f7e909d83612d0631751050e88f35582a83a
Austrian <114922365+0xAustrian@users.noreply.github.com>
2025-02-04 06:46:59 -0300

    fix: collision in ContractFactory (#64)

--- contracts/factories/ContractFactory.sol
+++ contracts/factories/ContractFactory.sol
@@ -97,7 +97,7 @@ contract ContractFactory {
         returns (address deployedContract)
     {
         // hash salt with the contract name and version
-        bytes32 salt = keccak256(abi.encodePacked(_contractName, _version));
+        bytes32 salt = keccak256(abi.encode(_contractName, _version));
 
         // ensure salt has not been used
         if (usedSalts[salt]) revert SALT_USED();
